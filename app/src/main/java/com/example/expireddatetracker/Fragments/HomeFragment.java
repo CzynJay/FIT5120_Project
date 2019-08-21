300aa77ef2bd69ed14868bc2ba247bed632df6ef
EricLiam94 <ericliamhjx@gmail.com>
2019-08-21 20:51:15 +1000

    v1.0.4

--- app/src/main/java/com/example/expireddatetracker/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/expireddatetracker/Fragments/HomeFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -82,8 +83,9 @@ public class HomeFragment extends Fragment {
     }
     private void search(String q)
     {
-        if(q.trim().length()==0)
+        if(q.trim().length()==0){
             searchBar.setHint("Please enter food name");
+            searchBar.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.shake));}
         else{
         Bundle bundle = new Bundle();
         bundle.putString("key",q);
