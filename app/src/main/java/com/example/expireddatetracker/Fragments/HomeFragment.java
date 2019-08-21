31ece5ae121c782e9e975bca1b69cfe38f24479b
EricLiam94 <ericliamhjx@gmail.com>
2019-08-21 16:54:53 +1000

    v1.0.2

--- app/src/main/java/com/example/expireddatetracker/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/expireddatetracker/Fragments/HomeFragment.java
@@ -8,10 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.expireddatetracker.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
@@ -21,8 +26,29 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View x  = inflater.inflate(R.layout.fragment_home, container, false);
-
-
+        String[] types = {"fruit","milk","meat","seafood","poultry","vegetable"};
+        Map<String, Integer> map = new HashMap<String,Integer>();
+        map.put("fruit",R.drawable.fruit);
+        map.put("milk",R.drawable.milk);
+        map.put("meat",R.drawable.meat);
+        map.put("seafood",R.drawable.seafood);
+        map.put("poultry",R.drawable.poultry);
+        map.put("vegetable",R.drawable.vegetable);
+        LinearLayout layout = x.findViewById(R.id.home_contain);
+        for(int temp=0;temp< types.length;temp++){
+            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View v = vi.inflate(R.layout.image_button, null);
+            ImageButton bt1 = v.findViewById(R.id.img1);
+            bt1.setImageResource(map.get(types[temp]));
+            TextView tx1  = v.findViewById(R.id.tx1);
+            tx1.setText(types[temp]);
+            temp++;
+            ImageButton bt2 = v.findViewById(R.id.img2);
+            bt2.setImageResource(map.get(types[temp]));
+            TextView tx2 = v.findViewById(R.id.tx2);
+            tx2.setText(types[temp]);
+            layout.addView(v);
+        }
         return x;
     }
 
