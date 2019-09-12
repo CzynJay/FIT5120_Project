d210e98a794ace82d22346bceb0457b16370514a
czynjay <czynjay@gmail.com>
2019-09-12 18:54:43 +0800

    Version 1.0.38 -Added return token of app instance (view through Logcat)

--- app/src/main/java/com/example/expireddatetracker/Fragments/ResultFragment.java
+++ app/src/main/java/com/example/expireddatetracker/Fragments/ResultFragment.java
@@ -156,6 +156,8 @@ public class ResultFragment extends Fragment{
         return v;
     }
 
+
+
     private void popUpWindow(String key,JSONArray jsonArray)
     {
         viewContainer.setVisibility(View.GONE);
